792e04610adbfed9cb3fe5a2cfbf9c7522234458
Ahmad Houri <houri@future-demand.com>
2022-06-06 11:47:10 +0200

    reorgnized celery module

--- app/api/api_v1/endpoints/utils.py
+++ app/api/api_v1/endpoints/utils.py
@@ -5,7 +5,7 @@ from pydantic.networks import EmailStr
 
 from app import models, schemas
 from app.api import deps
-from app.core.celery_app import celery_app
+from app.celery.celery_app import celery_app
 from app.utils import send_test_email
 
 router = APIRouter()
---
+++ app/worker/celeryconfig.py
@@ -0,0 +1,12 @@
+from app.core.config import settings
+
+# Broker settings.
+broker_url = settings.CELERY_BROKER_URL
+
+# List of modules to import when celery starts.
+imports = ('app.worker.tasks', )
+
+# Using the database to store task state and results.
+result_backend = settings.CELERY_RESULT_BACKEND
+
+task_annotations = {'tasks.add': {'rate_limit': '10/s'}}
